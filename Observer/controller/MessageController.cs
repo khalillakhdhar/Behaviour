e69be22a9a23419ceca28d8ddc34c82d2690226f
khalil <khalillakhdharatc@gmail.com>
2022-02-04 11:38:58 +0100

    code

---
+++ Observer/controller/MessageController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Behaviour.Observer.repository;
+using Behaviour.Observer.model;
+namespace Behaviour.Observer.controller
+{
+    class MessageController : MessageInterface
+    {
+        public void notifierUsers(Message message)
+        {
+            List<User> users = message.usersGroupe.users;
+
+
+
+        }
+
+        public void sendMessage(Message message)
+        {
+            Console.WriteLine("message envoyé");
+            this.notifierUsers(message);
+        }
+    }
+}
---
+++ Observer/repository/MessageInterface.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Behaviour.Observer.model;
+
+namespace Behaviour.Observer.repository
+{
+    interface MessageController
+    {
+        void sendMessage(Message message);
+        void notifierUsers(Message message);
+    }
+}
