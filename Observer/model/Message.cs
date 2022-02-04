e3b456bf287c1000544002030d48f7db90e1fd2a
khalil <khalillakhdharatc@gmail.com>
2022-02-04 11:30:21 +0100

    modelBehaviour

---
+++ Observer/model/Message.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Behaviour.Observer.model
+{
+    class Message
+    {
+        public string Texte { get; set; }
+        public string Date { get; set; }
+        public UserGroupe usersGroupe { get; set; }
+    }
+}
---
+++ Observer/model/UserGroupe.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Behaviour.Observer.model
+{
+    class UserGroupe
+    {
+        public List<User> users {get; set;}
+    }
+}
